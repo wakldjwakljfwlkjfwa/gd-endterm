8735c540e0a10090e2cb1937e707573aa454f951
MartyJ <mr.ershat@gmail.com>
2021-10-14 14:30:40 +0600

    sword attack added

--- Characters/Player/Player.gd
+++ Characters/Player/Player.gd
@@ -1,5 +1,7 @@
 extends Character
 
+onready var sword: Node2D = get_node("Sword")
+onready var sword_animation_player: AnimationPlayer = sword.get_node("SwordAnimationPlayer")
 func _process(_delta: float) -> void:
 	var mouse_direction: Vector2 = (get_global_mouse_position() - global_position).normalized()
 	
@@ -8,6 +10,13 @@ func _process(_delta: float) -> void:
 	elif mouse_direction.x < 0  and not animated_sprite.flip_h:
 		animated_sprite.flip_h = true
 		
+	sword.rotation = mouse_direction.angle()
+	if sword.scale.y == 1 and mouse_direction.x < 0:
+		sword.scale.y = -1
+	elif sword.scale.y == -1 and mouse_direction.x > 0:
+		sword.scale.y = 1
+	if Input.is_action_just_pressed("ui_attack") and not sword_animation_player.is_playing():
+		sword_animation_player.play("attack")
 func get_input() -> void:
 	mov_direction = Vector2.ZERO
 	if Input.is_action_pressed("ui_down"):
