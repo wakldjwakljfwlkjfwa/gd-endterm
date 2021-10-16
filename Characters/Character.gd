4bac109c02645c5decc9c2eea5a2bce7396abf81
dw <dw@dw>
2021-10-16 12:34:30 +0600

    implement take_damage and die methods

--- Characters/Character.gd
+++ Characters/Character.gd
@@ -10,12 +10,26 @@ onready var animated_sprite: AnimatedSprite = get_node("AnimatedSprite")
 
 var mov_direction: Vector2 = Vector2.ZERO
 var velocity: Vector2 = Vector2.ZERO
+var health: int = 0
+var health_max: int = 0
 
 func _physics_process(_delta: float) -> void:
 	velocity = move_and_slide(velocity)
 	velocity = lerp(velocity, Vector2.ZERO, FRICTION)
 
+func initialize(health_max: int = 10) -> void: 
+	self.health_max = health_max
+	self.health = health_max
+
 func move() -> void:
 	mov_direction = mov_direction.normalized()
 	velocity += mov_direction * acceleration
 	velocity = velocity.clamped(max_speed)
+
+func die() -> void:
+	queue_free()
+
+func take_damage(damage: int) -> void:
+	health -= damage
+	if health <= 0:
+		die()
