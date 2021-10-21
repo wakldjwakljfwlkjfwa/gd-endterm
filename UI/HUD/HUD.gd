b25a66d25f6856b501cc8f57f065143c6b1c1de2
dw <dw@dw>
2021-10-21 08:18:41 +0600

    change progress bar texture

--- UI/HUD/HUD.gd
+++ UI/HUD/HUD.gd
@@ -3,9 +3,9 @@ extends CanvasLayer
 var health = 100
 
 func _ready():
-	$HealthBar.set_health(health)
+	$HealthBar.set_progress(health)
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		health -= 25
-		$HealthBar.set_health(health)
+		$HealthBar.set_progress(health)
--- UI/HUD/HealthBar/HealthBar.gd
+++ UI/HUD/HealthBar/HealthBar.gd
@@ -1,11 +1,12 @@
 extends MarginContainer
 
-onready var progress_bar: ProgressBar = $HBoxContainer/MarginContainer/ProgressBar
+onready var progress_bar: TextureProgress = $HBoxContainer/MarginContainer/ProgressMargin/Progress
 onready var tween: Tween = $Tween
 
-func set_health(health: int) -> void:
-	print("take damage")
+var tween_speed: float = 0.5
+
+func set_progress(health: int) -> void:
 	tween.interpolate_property(
 		progress_bar, "value",
-		progress_bar.value, health, 1)
+		progress_bar.value, health, tween_speed)
 	tween.start()
