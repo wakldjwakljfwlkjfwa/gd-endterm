9b4740a595f4077847620e80bcc27a582404695f
MartyJ <mr.ershat@gmail.com>
2021-10-28 18:56:32 +0600

    Screen fix + cursor

--- Scenes/Game.gd
+++ Scenes/Game.gd
@@ -3,3 +3,9 @@ extends Node2D
 func _ready():
 	$BasicRoom/Slime.navigation = $Navigation2D
 	$BasicRoom/Slime.target = $BasicRoom/Player
+
+func _init() -> void:
+	var screen_size: Vector2 = OS.get_screen_size()
+	var window_size: Vector2 = OS.get_window_size()
+	
+	OS.set_window_position(screen_size * 0.5 - window_size * 0.5)
