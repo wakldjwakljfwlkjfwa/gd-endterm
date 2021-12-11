b60955d15d65c484d7f8b10463590201258dd7b2
6ernar667 <6ernar667@github.com>
2021-12-11 22:22:19 +0600

    create new level when level finished

--- Scenes/Game.gd
+++ Scenes/Game.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+onready var rooms_container = $RoomsContainer
+
 func _ready():
 	ConfigGame.game = self
 	
@@ -8,13 +10,10 @@ func _ready():
 	ConfigGame.player = player
 	ConfigGame.entity_container = $EntityContainer
 	
-	var generation = preload("res://Scripts/ProcGen/Generate.gd").new()
-	generation.init($Navigation2D, $RoomsContainer)
-	generation.generate()
+	level_create()
 	
 	$EntityContainer/Slime.navigation = $Navigation2D
 	$HUD/HealthBar.set_progress_max(ConfigGame.player.health_max)
-	$EntityContainer/Player.connect("hit", $HUD/HealthBar, "set_progress")
 
 func _init() -> void:
 	var screen_size: Vector2 = OS.get_screen_size()
@@ -23,4 +22,11 @@ func _init() -> void:
 	OS.set_window_position(screen_size * 0.5 - window_size * 0.5)
 
 func level_finished() -> void:
-	print("Level Finished")
+	level_create()
+
+func level_create() -> void:
+	for i in rooms_container.get_children():
+		i.queue_free()
+	var generation = preload("res://Scripts/ProcGen/Generate.gd").new()
+	generation.init($Navigation2D, $RoomsContainer)
+	generation.generate()
